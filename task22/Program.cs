c6a49a3a9bd16cb72739fcb5e79d8957bd9a0788
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 14:33:46 +0300

    Add task22

---
+++ task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Program takes a number as input and gives the squares of number in range [1, N] as output
+
+int num = 0;
+
+while (num < 1)
+{
+    Console.WriteLine("Add the positive number:");
+    num = int.Parse(Console.ReadLine());
+}
+
+int count = 1;
+int sq = 0;
+while (count <= num)
+{
+    sq = count * count;
+    Console.Write($"{sq}, ");
+    count++;
+}
