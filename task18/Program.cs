bfc6086169b997f09bff1d2051359804fa4a24c7
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 14:23:14 +0300

    Add task18 and task21

---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Program takes a number of quarter as input and gives a range of possible point coordinates as output
+
+int quat = 0;
+string xRange = "",
+    yRange = "";
+
+while ((quat < 1) || (quat > 4))
+{
+    Console.WriteLine("Add the number of quarter:");
+    quat = int.Parse(Console.ReadLine());
+}
+
+if ((quat == 1) || (quat == 4))
+{
+    xRange = "[0;+inf)";
+    if (quat == 1) yRange = xRange;
+    else yRange = "(-inf;0]";
+}
+else
+{
+    xRange = "(-inf;0]";
+    if (quat == 3) yRange = xRange;
+    else yRange = "[0;+inf)";
+}
+
+Console.WriteLine($"The range for quater {quat} is X: {xRange}, Y: {yRange}");
---
+++ task21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Find the distance between two points.
+
+Console.WriteLine("Add X1:");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Add Y1:");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Add X2:");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Add Y2:");
+double y2 = double.Parse(Console.ReadLine());
+
+double dist;
+
+dist = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+Console.WriteLine($"The distance between ({x1};{y1}) and ({x2};{y2}) is {dist}.");
